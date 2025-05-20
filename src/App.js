bada9e1ec2847024e306c5fd3830c6479cbf098f
amarilisdev <amarilis.dev@gmail.com>
2025-05-20 23:12:04 +0200

    fix ESLint error in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import ToDoList from "./ToDoList";
 
@@ -7,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <ToDoList />
-      
     </div>
   );
 }
